b7a199033b2ecba01fd391e13adc3d316f7b0ba4
inkarbegim <ik15@hw.ac.uk>
2020-09-24 18:49:59 +0100

    almost complete project
    HTML structure very flexible for adding new images with or without paragphraphs, idealy should be used with JSON files.
    
    Announcement bar and responsive view will be added soon.

---
+++ pages/footer.php
@@ -0,0 +1,90 @@
+<!DOCTYPE html>
+<html>
+    <head>
+	<link href="css/style.css" rel="stylesheet"/>
+    </head>
+    <body>
+        <footer class="thefooter">
+        <div class="footerContainer">
+            <div class="row">
+                <div class="footerCol left">
+                    <h5>Subscribe</h5>
+                    <h1>Email address</h1>
+                    <hr>
+                    <p>I would like to receive communications about Steams events, products and matter of cultural interest.</p>
+                </div>
+                
+                <div class="footerCol right">
+                    <h6>Catagory title</h6>
+                    <ul class="footerLinks">
+                        <li><a  href="#!">Catagory title</a></li>
+                        <li><a  href="#!">Catagory title</a></li>
+                        <li><a  href="#!">Catagory title</a></li>
+                        <li><a  href="#!">Catagory title</a></li>
+                    </ul>
+                </div>
+                
+                <div class="footerCol right">
+                    <h6>Catagory title</h6>
+                    <ul class="footerLinks">
+                        <li><a  href="#!">Catagory title</a></li>
+                        <li><a  href="#!">Catagory title</a></li>
+                        <li><a  href="#!">Catagory title</a></li>
+                        <li><a  href="#!">Catagory title</a></li>
+                    </ul>
+                </div>
+                
+                <div class="footerCol right">
+                    <h6>Social media</h6>
+                    <ul class="footerLinks">
+                        <li><a  href="#!">Instagram</a></li>
+                        <li><a  href="#!">twitter</a></li>
+                        <li><a  href="#!">LinkedIn</a></li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+        
+        <div class="footerCopyright">
+            <div class="footerContainer">
+            <a href="#!">© Streams</a>
+            </div>
+        </div>
+    </footer>
+    </body>
+</html>
+
+
+   
+    
+ 
+<!--Footer 
+			<footer>
+
+				<div class="adress">
+						<hr>
+						<ul>
+							<li>Mail us: <br>P.O. Box 1265 Bothell,<br> WA 98041-1265</li>			
+							<li>Find us: <br>10107 Woodinville Dr,<br> Bothell, WA 98011</li>							
+							<li>Phone us: <br>4 25-486-2732</li>
+							<li>Fax us: <br>425-487-6756</li>
+						</ul>
+				</div>
+
+				<div class="followUs">
+					<ul>
+						<li><h5>Follow Us:</h5></li>
+						<li><a href="https://www.facebook.com/Thorn-Ford-Dental-Laboratory-LLC-151426848252775/"><img src = "facebook.png" class="followUsImage"></a></li>
+					</ul>
+                   
+                </div>
+
+                <div class = "footer">
+                	 <ul>
+                        <li><h6>Copyright year (c)</h6></li>
+				    </ul>
+				 </div>
+
+			</footer>
+
+-->
---
+++ pages/header.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html>
+    <head>
+	<link href="css/style.css" rel="stylesheet"/>
+    </head>
+    <body>
+        
+    <header class="header">  
+<!--
+        <div class="headAnnouncement">
+            <div class="headerContainer">
+            <a href="#!">Announcement bar here with some text along the middle. Learn more</a>
+            </div>
+        </div>
+-->
+        <nav id="navbar">
+            <a href="login.php">Login</a>
+        </nav>
+    </header>
+    </body>
+</html>
+
+
